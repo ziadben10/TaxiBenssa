c4ef040c9667624ebd6e58a0d723e116e44c9c85
ziadben10 <63962998+ziadben10@users.noreply.github.com>
2025-08-05 10:46:37 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+}
+module.exports = nextConfig
